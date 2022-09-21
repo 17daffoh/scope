211d3ab14130be0a55b8f731c7ed726522b8b9d6
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2022-09-21 12:39:21 +0100

    Add project files.

---
+++ scope/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scope
+{
+    class Program
+    {
+        static string language = "english";
+        static void Main(string[] args)
+        {
+            showOff();
+            Console.WriteLine(language);
+        }
+
+        private static void showOff()
+        {
+            List<string> languages = new List<string> { "english", "german", "french", "spanish"};
+
+            foreach(string language in languages)
+            {
+                sayHello(language);
+            }
+        }
+
+        private static void sayHello(string language)
+        {
+            Console.Write($"you can greet people in {language} by saying ");
+            switch(language)
+            {
+                case "english":
+                    Console.WriteLine("hello");
+                    break;
+                case "german":
+                    Console.WriteLine("hallo");
+                    break;
+                case "french":
+                    Console.WriteLine("bonjour");
+                    break;
+                case "spanish":
+                    Console.WriteLine("hola");
+                    break;
+                default:
+                    Console.WriteLine("huh");
+                    break;
+            }
+        }
+    }
+}
